3b8fda3fab17695074e1a40d5899d27b8045626c
Leo Krasnov <leonid.krasnov0@gmail.com>
2017-02-27 16:59:37 -0600

    updating template

--- docs/js/wiki.js
+++ docs/js/wiki.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $("#searchForm").submit(function(event) {
-    var search = $("#searchText").val();
+    const search = $("#searchText").val();
     $.ajax({
       cache: false,
       url: "https://en.wikipedia.org/w/api.php",
@@ -27,31 +27,22 @@ $(document).ready(function() {
 });
 
 function updateSearchResults(pages) {
-  clearResults();
-  var results = [];
-
-  Object.keys(pages).forEach(key => {
-    results.push(pages[key]);
-  });
-
-  results.sort(function(a, b) {
-    return a.index - b.index;
-  });
-
-  results.forEach(function(result) {
-    appendSearchResult(result);
-  });
-}
-
-function appendSearchResult(result) {
-  $("#results").append(
-    '<a href="' + result.fullurl + '" target="_blank" class="list-group-item list-group-item-action mt-2">'+
-      '<h4>' + result.title + '</h4>'+
-      '<p class="text-justify">' + result.extract + '</p>'+
-    '</a>'
-  );
-}
-
-function clearResults() {
   $("#results").empty();
+  const results = [];
+
+  Object.keys(pages).forEach(key => results.push(pages[key]));
+
+  results.sort((a, b) => a.index - b.index);
+  results.forEach(appendSearchResult);
 }
+
+const appendSearchResult = result => (
+  $("#results").append(
+    `<a href="${result.fullurl}" target="_blank" class="list-group-item list-group-item-action mt-2">
+      <h4>${result.title}</h4>
+      <div class="container p-0">
+        <p class="text-justify text-left">${result.extract}</p>
+      </div>
+    </a>`
+  )
+);
